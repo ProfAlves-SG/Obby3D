761b21ca0d4b1ef80a2662266c0cf6a92e25089f
Guilherme Alves <208266960+ProfAlves-SG@users.noreply.github.com>
2026-02-21 17:09:33 -0300

    Finish cam control

--- Assets/Scripts/PlayerCamera3D.cs
+++ Assets/Scripts/PlayerCamera3D.cs
@@ -1,21 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCamera3D : MonoBehaviour
+public class ThirdPersonCam : MonoBehaviour
 {
-    private void Update()
+    [Header("References")]
+    public Transform player;
+    public Transform playerObj;
+    public Transform orientation;
+
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        // Capturar o Mouse quando o jogo começar
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Controle da captura do mouse 
+        
+        // Caso apertar Esc, solte o mouse
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
         }
 
+        // Caso apertar o botão esquerdo do mouse na tela e o mouse não estiver capturado, capture
         if (Input.GetMouseButtonDown(0) && Cursor.lockState != CursorLockMode.Locked)
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
+        
+        // Rotacionar o objeto Orientation do Player
+
+        Vector3 viewDirection = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDirection.normalized;
+
+        // Rotacionar o modelo do Player de acordo com os inputs de movimento
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 inputDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (inputDirection != Vector3.zero)
+        {
+            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
+        }
+
     }
 }
